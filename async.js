84ea675297bfb60885995ec99554c9d4f234ac19
ShovanTheMan <shovan2192@gmail.com>
2024-02-12 17:06:13 +0600

    asynchronus added

---
+++ async.js
@@ -0,0 +1,14 @@
+console.log(1);
+console.log(2);
+setTimeout(() => {
+    console.log('lazy dog');
+}, 4000);
+// 4000 milisecond = 4 second por show korbe 
+// settimeout,fetch they cannot obtain serial they come in the end in serial.
+console.log(4);
+console.log(5);
+console.log(6);
+
+function doSomething(){
+    console.log(3);
+}
